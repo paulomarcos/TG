d103f8a0d5b1a4083dbd59cbedc6a8213942cc71
Paulo <paulomarcosdj@outlook.com>
2015-10-05 17:53:48 -0300

    Add Professor view

--- RailsTeste/SISLETRON/app/controllers/professors_controller.rb
+++ RailsTeste/SISLETRON/app/controllers/professors_controller.rb
@@ -1,6 +1,19 @@
-class ProfessorController < ApplicationController
+class ProfessorsController < ApplicationController
   def index
     @professors = Professor.all
+  end
+
+  def show
+    @professor = Professor.find(params[:id])
+
+    @turmas = @professor.turmas
+
+    @plano = Plano.where(professor_id: @professor.id).pluck('projeto_id')
+    @projetos_plano = Projeto.where(id: @plano)
+
+    @execucao = Execucao.where(professor_id: @professor.id).pluck('projeto_id')
+    @projetos_execucao = Projeto.where(id: @execucao)
+
   end
 
 end
